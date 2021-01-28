5e3940a694aba8d198a50ffbd2aa66609d63815a
QueenTesa Fuggett <qfuggett@umich.edu>
2021-01-28 14:31:45 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends: []}, 
+    action){
+    switch(action.type) {
+        case 'ADD_FRIEND':
+            return {...state, friends: [...state.friends, action.friend]}
+        case 'REMOVE_FRIEND':
+            const removeFriend = state.friends.map(remove => remove.id)
+            return {
+                ...state, friends: state.friends.filter(friend => friend.id !== removeFriend)
+            }
+        default:
+            return state;
+    }
 }
+
